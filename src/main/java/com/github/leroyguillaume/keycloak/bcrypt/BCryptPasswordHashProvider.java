d6786117c63e153e65dbb6a41d0ca8d38dc9bc8c
Georgi Matev <georgi.matev@gmail.com>
2019-12-20 15:06:55 -0800

    Migrate to new interfaces for Keycloak 8.0.1

--- src/main/java/com/github/leroyguillaume/keycloak/bcrypt/BCryptPasswordHashProvider.java
+++ src/main/java/com/github/leroyguillaume/keycloak/bcrypt/BCryptPasswordHashProvider.java
@@ -1,15 +1,10 @@
 package com.github.leroyguillaume.keycloak.bcrypt;
 
 import org.jboss.logging.Logger;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import org.keycloak.credential.CredentialModel;
+import org.keycloak.models.credential.PasswordCredentialModel;
 import org.keycloak.credential.hash.PasswordHashProvider;
 import org.keycloak.models.PasswordPolicy;
-import org.keycloak.models.UserCredentialModel;
 import org.mindrot.jbcrypt.BCrypt;
-
-import java.io.IOException;
 
 /**
  * @author <a href="mailto:pro.guillaume.leroy@gmail.com">Guillaume Leroy</a>
@@ -28,38 +23,28 @@ public class BCryptPasswordHashProvider implements PasswordHashProvider {
     }
 
     @Override
-    public boolean policyCheck(PasswordPolicy policy, CredentialModel credential) {
+    public boolean policyCheck(PasswordPolicy policy, PasswordCredentialModel credential) {
         int policyHashIterations = policy.getHashIterations();
         if (policyHashIterations == -1) {
             policyHashIterations = defaultIterations;
         }
 
-        return credential.getHashIterations() == policyHashIterations && providerId.equals(credential.getAlgorithm());
+        return credential.getPasswordCredentialData().getHashIterations() == policyHashIterations
+                && providerId.equals(credential.getPasswordCredentialData().getAlgorithm());
+    }
+
+    @Override
+    public PasswordCredentialModel encodedCredential(String rawPassword, int iterations) {
+        String encodedPassword = encode(rawPassword, iterations);
+
+        // bcrypt salt is stored as part of the encoded password so no need to store salt separately
+        return PasswordCredentialModel.createFromValues(providerId, new byte[0], iterations, encodedPassword);
     }
 
     @Override
     public String encode(String rawPassword, int iterations) {
-        int logRounds = iterations == -1 ? iterationsToLogRounds(defaultIterations) : iterationsToLogRounds(iterations);
-        return BCrypt.hashpw(rawPassword, BCrypt.gensalt(logRounds));
-    }
-
-    @Override
-    public void encode(String rawPassword, int iterations, CredentialModel credential) {
-        if (iterations == -1) {
-            iterations = defaultIterations;
-        }
-
-        String salt = BCrypt.gensalt(iterationsToLogRounds(iterations));
-        String password = BCrypt.hashpw(rawPassword, salt);
-
-        credential.setAlgorithm(providerId);
-        credential.setType(UserCredentialModel.PASSWORD);
-        credential.setHashIterations(iterations);
-        credential.setValue(password);
-
-        // Salt encoding is modified base64 so standard decode does not work
-        // No need to actually record salt separately
-        credential.setSalt(new byte[0]);
+        String salt = generateBCryptSalt(iterations);
+        return BCrypt.hashpw(rawPassword, salt);
     }
 
     @Override
@@ -68,13 +53,19 @@ public class BCryptPasswordHashProvider implements PasswordHashProvider {
     }
 
     @Override
-    public boolean verify(String rawPassword, CredentialModel credential) {
-        return BCrypt.checkpw(rawPassword, credential.getValue());
+    public boolean verify(String rawPassword, PasswordCredentialModel credential) {
+        return BCrypt.checkpw(rawPassword, credential.getPasswordSecretData().getValue());
+    }
+
+    private String generateBCryptSalt(int iterations) {
+        int logRounds = iterations == -1 ? iterationsToLogRounds(defaultIterations) : iterationsToLogRounds(iterations);
+        return BCrypt.gensalt(logRounds);
     }
 
     private int iterationsToLogRounds(int iterations) {
          // bcrypt uses 2**log2_rounds with a min of 4 and max of 30 log rounds
-        return Math.max(MIN_BCRYPT_LOG_ROUNDS, Math.min(MAX_BCRYPT_LOG_ROUNDS,
-                (int) Math.round(Math.log(iterations) / Math.log(2) + 1)));
+         // Always round up if iterations represent a fractional number of rounds
+        return Math.max(MIN_BCRYPT_LOG_ROUNDS, Math.min(MAX_BCRYPT_LOG_ROUNDS, 
+                (int) Math.ceil(Math.log(iterations) / Math.log(2))));
     }
 }
