904d8496ebd9b7ba4c889bcaaf5418306338b8a7
ajoel24 <drew.devops@gmail.com>
2022-07-18 01:32:46 +0530

    - Minor refactors in BCryptPasswordHashProvider - Add tests - Upgrade Keycloak version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,20 @@ plugins {
 }
 
 group = "com.github.leroyguillaume"
-version = "1.5.0"
+version = "1.5.1"
 
 repositories {
     mavenCentral()
 }
 
+configurations {
+    testImplementation.get().apply {
+        extendsFrom(configurations.compileOnly.get())
+    }
+}
+
 dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     val bcryptVersion = "0.9.0"
     val jbossLoggingVersion = "3.4.1.Final"
     val keycloakVersion = project.property("dependency.keycloak.version")
@@ -44,5 +51,9 @@ tasks {
 
     wrapper {
         gradleVersion = "6.4"
+    }
+
+    test {
+        useJUnitPlatform()
     }
 }
